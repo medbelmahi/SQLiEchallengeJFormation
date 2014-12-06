80ac850eed349e495665f53ec9d9c2590e2fe1b4
Mouad <Mouad.fkr@gmail.com>
2014-12-06 00:41:23 +0000

    sessionList update

--- SQLiEchallengeJFormation/src/main/java/com/sqli/echallenge/jformation/web/responsableformation/SessionFormationListAction.java
+++ SQLiEchallengeJFormation/src/main/java/com/sqli/echallenge/jformation/web/responsableformation/SessionFormationListAction.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Controller;
 
 import com.opensymphony.xwork2.validator.annotations.RequiredFieldValidator;
 import com.sqli.echallenge.jformation.metier.SessionFormationMetier;
+import com.sqli.echallenge.jformation.metier.UtilisateurMetier;
 import com.sqli.echallenge.jformation.model.entity.SessionFormation;
+import com.sqli.echallenge.jformation.model.entity.Utilisateur;
 import com.sqli.echallenge.jformation.web.SqliActionSupport;
 
 /**
@@ -23,13 +25,19 @@ public class SessionFormationListAction extends SqliActionSupport {
 
 	@Autowired
 	public SessionFormationMetier sessionFormationMetier;
+	@Autowired
+	public UtilisateurMetier formateurMetier;
 	
 	private Long idFormation;
 	private List<SessionFormation> sessions;
+	private List<Utilisateur> formateurs;
 	
 	@Override
 	public String execute() throws Exception {
 		try {
+			//get List formateurs (for select(html) of addSessionModel)
+			formateurs = formateurMetier.getFormateurs();
+			
 			//get All Sessions of Formation
 			sessions = sessionFormationMetier.getAll(idFormation);
 			System.out.println(">>Sessions: " + sessions);//Debug
@@ -58,6 +66,14 @@ public class SessionFormationListAction extends SqliActionSupport {
 
 	public void setSessions(List<SessionFormation> sessions) {
 		this.sessions = sessions;
+	}
+
+	public List<Utilisateur> getFormateurs() {
+		return formateurs;
+	}
+
+	public void setFormateurs(List<Utilisateur> formateurs) {
+		this.formateurs = formateurs;
 	}
 
 }
